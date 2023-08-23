408ca5a8743b7ccf31a239720b7f040c4fa17bcd
Paulo Previlato <paulo.previlato@gmail.com>
2023-08-23 18:22:34 -0400

    Implementação da classe Repository e Service para Usuario.

---
+++ src/main/java/api/voll/med/api/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package api.voll.med.api.repository;
+
+import api.voll.med.api.domain.usuario.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByLogin(String login);
+}
