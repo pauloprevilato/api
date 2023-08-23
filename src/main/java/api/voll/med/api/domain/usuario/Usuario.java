12265330d88cb1643a2c5b8762b3fff2b9a5c76b
Paulo Previlato <paulo.previlato@gmail.com>
2023-08-23 18:11:15 -0400

    Adicionado no projeto spring-security, criado entidade de Usuario e também a nova versão da migration.

---
+++ src/main/java/api/voll/med/api/domain/usuario/Usuario.java
@@ -0,0 +1,22 @@
+package api.voll.med.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "Usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
