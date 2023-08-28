6d21e25fda5b8c1229f033fe17a4165d2ed2a87c
Paulo Previlato <paulo.previlato@gmail.com>
2023-08-28 19:42:43 -0400

    Implementação do token de acesso para usuario.

--- src/main/java/api/voll/med/api/controller/AutenticacaoController.java
+++ src/main/java/api/voll/med/api/controller/AutenticacaoController.java
@@ -1,6 +1,9 @@
 package api.voll.med.api.controller;
 
 import api.voll.med.api.domain.usuario.DadosAutenticacao;
+import api.voll.med.api.domain.usuario.Usuario;
+import api.voll.med.api.infra.security.DadosTokenJWT;
+import api.voll.med.api.service.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +21,15 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados)  {
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authenticaon = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 }
---
+++ src/main/java/api/voll/med/api/infra/security/DadosTokenJWT.java
@@ -0,0 +1,4 @@
+package api.voll.med.api.infra.security;
+
+public record DadosTokenJWT(String token) {
+}
