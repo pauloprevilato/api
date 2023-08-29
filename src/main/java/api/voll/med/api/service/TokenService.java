5a92c73d326692fd3e7d708d4446cf8f4eadac5e
Paulo Previlato <paulo.previlato@gmail.com>
2023-08-29 19:02:45 -0400

    Implementação do filter, recuperacao, validacao de token e autenticacao para o usuario.

--- src/main/java/api/voll/med/api/service/TokenService.java
+++ src/main/java/api/voll/med/api/service/TokenService.java
@@ -4,6 +4,7 @@ import api.voll.med.api.domain.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +27,19 @@ public class TokenService {
                         .sign(algoritmo);
         } catch (JWTCreationException exception){
         throw new RuntimeException("erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
